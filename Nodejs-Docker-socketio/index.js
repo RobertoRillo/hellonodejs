cf605ad96d25abd6557b8d17911e7b18689b212b
Roberto Rillo <89648261+RobertoRillo@users.noreply.github.com>
2024-04-21 10:58:53 -0600

    carpeta nueva

---
+++ Nodejs-Docker-socketio/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const port = 3000
+
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>');
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
